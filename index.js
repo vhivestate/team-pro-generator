80bda3880c0baf51e5cf8e8aab09864f65d7dc99
vhivestate <garcia.valeria001@gmail.com>
2022-06-07 15:14:20 -0600

    generate html

--- index.js
+++ index.js
@@ -78,7 +78,78 @@ function generateMember() {
                   return false;
               }
           }
-      }])
+          //loop add members
+      }]).then(function({userRoleInput}) {
+        console.log(userRoleInput)
+        if(role === "Manager"){
+            let newManager = new Manager(name, memberId, email, userRoleInput)
+            teamEmp.push(newManager)
+            console.log(teamEmp)
+        }
+        if(role === "Engineer"){
+            let newEngineer = new Engineer(name, memberId, email, userRoleInput)
+            teamEmp.push(newEngineer)
+            console.log(teamEmp)
+        }
+        if(role === "Intern"){
+            let newIntern = new Intern(name, memberId, email, userRoleInput)
+            teamEmp.push(newIntern)
+            console.log(teamEmp)
+        }
+        //asks user if they would like to add new user
+        inquirer.prompt({name: "choice", type: "confirm", message: "Would you like to add another team member?" }).then(function({choice}){
+            if(choice) {
+                generateMember()
+            } else {
+                let empHtml = ""
+
+                for (let index = 0; index < teamEmp.length; index++) {
+                    let special = ""
+                    if(teamEmp[index].getRole() === "Manager"){
+                        special = teamEmp[index].getOfficeNumber()
+                    }
+                    if(teamEmp[index].getRole() === "Engineer"){
+                        special = teamEmp[index].getGithub()
+                    }
+                    if(teamEmp[index].getRole() === "Intern"){
+                        special = teamEmp[index].getSchool()
+                    }
+                    empHtml += `
+                    <div>
+                    <p>${teamEmp[index].getName()}</p>
+                    <p>${teamEmp[index].getRole()}</p>
+                    <ul>
+                    <li>${teamEmp[index].getId()}</li>
+                    <li>${teamEmp[index].getEmail()}</li>
+                    <li>${special}</li>
+                    </ul>
+
+                    </div>
+                    `
+                }
+                let html = `<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                    <title>Document</title>
+                </head>
+                <body>
+                    ${empHtml}
+                </body>
+                </html>`
+                console.log('Team has been built!')
+                // generates html
+                fs.writeFile('./index.html', html, function(err){
+                    if(err){
+                        console.log(err)
+                    }
+                })
+            }
+        })
+      })
   })
 };
 
